122d787ab20b57f4a53f2bcd7913adb19de68ccb
Evan Miracle <robertevanmiracle@gmail.com>
2018-02-21 15:24:02 -0500

    test commit from local

---
+++ cooridor_nav.py
@@ -0,0 +1,32 @@
+# cooridor and movement
+
+# available movement
+
+
+width=4
+length=2
+current_environment=[width, length]
+
+print(current_environment)
+
+# redefine coordinates must redefine current_environment too
+
+width=3
+length=7
+current_environment=[width, length]
+
+print(current_environment)
+print(current_environment[0])
+
+
+# start_width=0
+# start_length=0
+# move_width=2
+# move_length=2
+# forward
+# left 
+# right
+# back
+# final_width=
+# final_length=
+# current_position=[0,0]
